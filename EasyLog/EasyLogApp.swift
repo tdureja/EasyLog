94e3794d58b72413fc8f7e1a4ea62fa8f454e650
tdureja <durejatejvir@gmail.com>
2025-12-30 09:09:16 -0800

    Initial Commit

---
+++ EasyLog/EasyLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyLogApp.swift
+//  EasyLog
+//
+//  Created by Tejvir Dureja on 12/30/25.
+//
+
+import SwiftUI
+
+@main
+struct EasyLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EasyLogTests/EasyLogTests.swift
@@ -0,0 +1,16 @@
+//
+//  EasyLogTests.swift
+//  EasyLogTests
+//
+//  Created by Tejvir Dureja on 12/30/25.
+//
+
+import Testing
+
+struct EasyLogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
